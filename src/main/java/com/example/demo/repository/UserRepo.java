65552ff540970698ed760aa31b3ef41ed2dd38cf
Soumya Biswas <soumyabiet@gmail.com>
2021-02-10 12:26:55 +0530

    Removed findByEmail

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -1,7 +1,5 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.example.demo.model.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-	Optional<User> findByEmail(String email);
+	
 }
